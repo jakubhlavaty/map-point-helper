5bddb729bb479815bc6fcb27744aa7f4a029b2b6
Jakub Hlavaty <jakub.hlavaty@zf.com>
2017-07-19 16:29:05 +0200

    added generator service and UUID generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 import {MapPointServiceService} from "./map-point-service.service";
 import {ExportService} from "./export.service";
+import {GeneratorService} from "./generator.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  providers: [MapPointServiceService, ExportService]
+  providers: [MapPointServiceService, ExportService, GeneratorService]
 })
 export class AppComponent {
   title = 'app';
--- src/app/export.service.ts
+++ src/app/export.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
 import {marker} from "./map-point-service.service";
+import {GeneratorService} from "./generator.service";
+const LAT = "${lat}";
+const LNG = "${lng}";
 
 @Injectable()
 export class ExportService {
 
-  constructor() { }
+  constructor(private generatorService: GeneratorService) { }
 
   export(markers: marker[], itemTemplate: string, separator: string) {
     let itemList: string[] = [];
     for (var marker of markers) {
       let lat = marker.lat;
       let lng = marker.lng;
-      itemList.push(itemTemplate.replace("${lat}", lat.toString()).replace("${lng}", lng.toString()));
+
+      let item = itemTemplate;
+      while (item.indexOf(LAT) > -1) {
+        item = item.replace(LAT, lat.toString());
+      }
+      while (item.indexOf(LNG) > -1) {
+        item = item.replace(LNG, lng.toString());
+      }
+
+      item = this.generatorService.applyGenerators(item);
+
+      itemList.push(item);
     }
     return itemList.join(separator);
   }
