37b75d8aa4ff6e2df5594b0fab99905d504a5964
jakub.hlavaty <jakub.hlavaty@gmail.com>
2017-08-03 23:48:09 +0200

    start with an example template

--- src/app/template-editor/template-editor.component.ts
+++ src/app/template-editor/template-editor.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {MapPointServiceService} from "../map-point-service.service";
 import {ExportService} from "../export.service";
 
+const EXAMPLE_TEMPLATE:string = '{\n   index: ${i},\n   lat: "${lat}",\n   lng: "${lng}",\n   UUID: "${gen:uuid}"\n }';
+
 @Component({
   selector: 'app-template-editor',
   templateUrl: './template-editor.component.html',
@@ -9,7 +11,7 @@ import {ExportService} from "../export.service";
 })
 export class TemplateEditorComponent implements OnInit {
   result: string = '';
-  templateString: string = '';
+  templateString: string = EXAMPLE_TEMPLATE;
 
   constructor(private mapService: MapPointServiceService, private exportService: ExportService) { }
 
